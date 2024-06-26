fa1699ba28ab3983cc2d9926bd87f1a3744518d3
OmerHafeez <123534593+OmerHafeez@users.noreply.github.com>
2024-06-26 16:07:33 +0500

    Add files via upload

---
+++ is-lowercase-character-ternary.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+char A;
+
+cout << "Enter a character";
+cin >> A;
+
+if((A>='a')&&(A<='z'))
+cout << "It is smallcase letter"<<endl;
+else
+cout << "It is not smallcase letter"<<endl;
+
+
+}
